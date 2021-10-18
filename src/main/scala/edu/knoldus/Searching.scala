d788b71d29c713fcc55f988d2fefea2873fda7b3
akashkr17 <akash.kumar@knoldus.com>
2021-10-18 16:18:36 +0530

    Initial commit

---
+++ src/main/scala/edu/knoldus/Searching.scala
@@ -0,0 +1,9 @@
+package edu.knoldus
+
+class Searching {
+
+  def binarySearch(array: Array[Int], elem: Int): Boolean = ???
+
+  def linearSearch(array: Array[Int], elem: Int): Boolean = ???
+
+}
---
+++ src/main/scala/edu/knoldus/Sorting.scala
@@ -0,0 +1,11 @@
+package edu.knoldus
+
+class Sorting {
+
+  def insertionSort(array: Array[Int]): Array[Int] = ???
+
+  def selectionSort(array: Array[Int]): Array[Int] = ???
+
+  def bubbleSort(array: Array[Int]): Array[Int] = ???
+
+}
