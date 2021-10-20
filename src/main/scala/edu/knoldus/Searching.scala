d4dffea8af4e7a63abc709e96bfb95a3cc573fe8
akashkr17 <akash.kumar@knoldus.com>
2021-10-20 17:32:58 +0530

    Add the logic for sorting and searching

--- src/main/scala/edu/knoldus/Searching.scala
+++ src/main/scala/edu/knoldus/Searching.scala
@@ -2,8 +2,31 @@ package edu.knoldus
 
 class Searching {
 
-  def binarySearch(array: Array[Int], elem: Int): Boolean = ???
+  def binarySearch(array: Array[Int], elem: Int): Boolean = {
 
-  def linearSearch(array: Array[Int], elem: Int): Boolean = ???
+    @scala.annotation.tailrec
+    def binarySearchHelper(array: Array[Int],
+                           elem: Int,
+                           low: Int,
+                           high: Int): Boolean = if (low > high) { false }
+                           else {
+                             val middle = low + (high - low) / 2
+                             array match {
+                               case array if array(middle) == elem => true
+                               case array if array(middle) < elem => binarySearchHelper(array, elem, middle + 1, high)
+                               case array if array(middle) > elem =>
+                                 binarySearchHelper(array, elem, low, middle - 1)
+                             }
+                           }
+    binarySearchHelper(array, elem, 0, array.length - 1)
+  }
+
+  def linearSearch(array: Array[Int], elem: Int): Boolean = {
+    array match {
+      case a if a.isEmpty      => false
+      case a if a.head == elem => true
+      case _                   => linearSearch(array.tail, elem)
+    }
+  }
 
 }
--- src/main/scala/edu/knoldus/Sorting.scala
+++ src/main/scala/edu/knoldus/Sorting.scala
@@ -1,11 +1,50 @@
 package edu.knoldus
-
 class Sorting {
 
-  def insertionSort(array: Array[Int]): Array[Int] = ???
+  def insertionSort(array: Array[Int]): Array[Int] = {
+    val size = array.length
+    for (step <- 1 until size) {
+      val key = array(step)
+      var j = step - 1
+      while ({
+        j >= 0 && key < array(j)
+      }) {
+        array(j + 1) = array(j)
+        j -= 1
+      }
+      array(j + 1) = key
+    }
+    array
+  }
 
-  def selectionSort(array: Array[Int]): Array[Int] = ???
+  def selectionSort(array: Array[Int],
+                    first: Int = 0,
+                    second: Int = 0): Array[Int] = {
 
-  def bubbleSort(array: Array[Int]): Array[Int] = ???
-
+    if (first >= array.length - 1) {
+      array
+    } else if (second >= array.length - 1) {
+      selectionSort(array, first + 1)
+    } else {
+      for (j <- first until array.length) {
+        if (array(j) < array(second)) {
+          val temp = array(second)
+          array(second) = array(j)
+          array(j) = temp
+        }
+      }
+      selectionSort(array, first, second + 1)
+    }
+    array
+  }
+  def bubbleSort(array: Array[Int]): Array[Int] = {
+    for (i <- 0 until array.length - 1; j <- 0 until array.length - 1 - i) {
+      if (array(j) > array(j + 1)) {
+        val tempElem = array(j)
+        array(j) = array(j + 1)
+        array(j + 1) = tempElem
+      }
+    }
+    array
+  }
 }
